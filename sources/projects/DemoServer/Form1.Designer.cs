2fc4edb21414120b7518b2eda7e2cbad6d27bacb
lanarchyste <julien.binet@supinfo.com>
2013-04-16 18:18:34 +0200

    Developpement de la demo serveur

--- sources/projects/DemoServer/Form1.Designer.cs
+++ sources/projects/DemoServer/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rtbMessageReceived = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Démarrer";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Arrêter";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 62);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(353, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(371, 60);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Envoyer";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rtbMessageReceived
+            // 
+            this.rtbMessageReceived.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbMessageReceived.Location = new System.Drawing.Point(12, 88);
+            this.rtbMessageReceived.Name = "rtbMessageReceived";
+            this.rtbMessageReceived.ReadOnly = true;
+            this.rtbMessageReceived.Size = new System.Drawing.Size(434, 167);
+            this.rtbMessageReceived.TabIndex = 4;
+            this.rtbMessageReceived.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 267);
+            this.Controls.Add(this.rtbMessageReceived);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Demo - Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox rtbMessageReceived;
     }
 }
 
--- sources/projects/DemoServer/Form1.cs
+++ sources/projects/DemoServer/Form1.cs
@@ -1,19 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using AsyncNamedPipes;
+using AsyncNamedPipes.Event;
+using AsyncNamedPipes.Message;
 
 namespace DemoServer
 {
     public partial class Form1 : Form
     {
+        private readonly NamedPipeServer _pipeServer;
+
         public Form1()
         {
             InitializeComponent();
+
+            _pipeServer = new NamedPipeServer("pipeDemo", 255);
+            _pipeServer.MessageReceived += MessageReceived;
+        }
+
+        private void MessageReceived(object sender, MessageEventArgs args)
+        {
+            rtbMessageReceived.Text += args.Message + Environment.NewLine;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _pipeServer.Connect();
+
+            btnStop.Enabled = _pipeServer.IsRunning;
+            btnStart.Enabled = !_pipeServer.IsRunning;
+            btnSend.Enabled = _pipeServer.IsRunning;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _pipeServer.Disconnect();
+
+            btnStop.Enabled = _pipeServer.IsRunning;
+            btnStart.Enabled = !_pipeServer.IsRunning;
+            btnSend.Enabled = _pipeServer.IsRunning;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (_pipeServer.ClientsConnectedCount > 0)
+            {
+                var message = new GenericMessage("server", "all", DateTime.Now, typeof (string), txtMessage.Text);
+                _pipeServer.SendMessage(message);
+            }
+            else
+                rtbMessageReceived.Text += "Impossible d'envoyer le message : aucun client" + Environment.NewLine;
         }
     }
 }
