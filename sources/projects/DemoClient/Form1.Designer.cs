5a5c235795712a26589d1970e0e44e1d1fc631ba
lanarchyste <julien.binet@supinfo.com>
2013-04-16 18:18:18 +0200

    Developpement de la demo client

--- sources/projects/DemoClient/Form1.Designer.cs
+++ sources/projects/DemoClient/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rtbMessageReceived = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(80, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connecter";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(98, 12);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(80, 23);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Déconnecter";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 62);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(353, 20);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(371, 60);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Envoyer";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rtbMessageReceived
+            // 
+            this.rtbMessageReceived.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbMessageReceived.Location = new System.Drawing.Point(12, 88);
+            this.rtbMessageReceived.Name = "rtbMessageReceived";
+            this.rtbMessageReceived.ReadOnly = true;
+            this.rtbMessageReceived.Size = new System.Drawing.Size(434, 167);
+            this.rtbMessageReceived.TabIndex = 5;
+            this.rtbMessageReceived.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 267);
+            this.Controls.Add(this.rtbMessageReceived);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Demo - Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox rtbMessageReceived;
     }
 }
 
--- sources/projects/DemoClient/Form1.cs
+++ sources/projects/DemoClient/Form1.cs
@@ -1,19 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using AsyncNamedPipes;
+using AsyncNamedPipes.Event;
 
 namespace DemoClient
 {
     public partial class Form1 : Form
     {
+        private readonly NamedPipeClient _pipeClient;
+
         public Form1()
         {
             InitializeComponent();
+
+            _pipeClient = new NamedPipeClient("pipeDemo");
+            _pipeClient.MessageReceived += MessageReceived;
+        }
+
+        private void MessageReceived(object sender, MessageEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            var success = _pipeClient.Connect(1000);
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
